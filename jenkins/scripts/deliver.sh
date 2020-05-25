0e0069710dd9343b0b91b7e70e6a79b217904efb
Blain Cribb <blain.cribb@ad.infosys.com>
2020-05-25 14:51:39 +1200

    Changed the deliver.sh to be less verbose

--- jenkins/scripts/deliver.sh
+++ jenkins/scripts/deliver.sh
@@ -1,26 +1,20 @@
 #!/usr/bin/env bash
 
-echo 'The following Maven command installs your Maven-built Java application'
-echo 'into the local Maven repository, which will ultimately be stored in'
-echo 'Jenkins''s local Maven repository (and the "maven-repository" Docker data'
-echo 'volume).'
+echo 'Creating .jar to store in Jenkins maven-repository'
 set -x
 mvn jar:jar install:install help:evaluate -Dexpression=project.name
 set +x
 
-echo 'The following complex command extracts the value of the <name/> element'
-echo 'within <project/> of your Java/Maven project''s "pom.xml" file.'
+echo 'Get the name of the project from the pom.xml'
 set -x
 NAME=`mvn help:evaluate -Dexpression=project.name | grep "^[^\[]"`
 set +x
 
-echo 'The following complex command behaves similarly to the previous one but'
-echo 'extracts the value of the <version/> element within <project/> instead.'
+echo 'Get the version of the project from the pom.xml'
 set -x
 VERSION=`mvn help:evaluate -Dexpression=project.version | grep "^[^\[]"`
 set +x
 
-echo 'The following command runs and outputs the execution of your Java'
-echo 'application (which Jenkins built using Maven) to the Jenkins UI.'
+echo 'Runs the project and outputs it to the Maven UI'
 set -x
 java -jar target/${NAME}-${VERSION}.jar
